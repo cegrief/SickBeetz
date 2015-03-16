a95854e684ef38f84abe09d6c57d0ca283f5ad7e
cegrief <cegrief@gmail.com>
2015-03-15 19:20:53 -0500

    Implemented segmenter using librosa onset detection segmenter called by calling segment_audio(signal, sample_rate)
    Outputs an array of segments and an array of the times they are at.
    
    Run segmenter.py "path-to-audio-file" to try it! If you don't have an audio file handy it will default to test1.wav

--- segmentr.py
+++ segmentr.py
@@ -1,21 +1,50 @@
 # audio segmenter attempt using onset detection via librosa
 
 import librosa
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+HOP_LENGTH = 256
 
 
 def segment_audio(signal, sr):
-    o_env = librosa.onset.onset_strength(signal, sr)
-    onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
-    onset_times = librosa.frames_to_time(onset_frames, sr)
-    print(onset_frames)
-    print(onset_times)
+
+    o_env = librosa.onset.onset_strength(y=signal, sr=sr, centering=False, hop_length=HOP_LENGTH)
+    onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, hop_length=HOP_LENGTH)
+    onset_times = librosa.frames_to_time(onset_frames, sr=sr, hop_length=HOP_LENGTH)
+
+    segments = []
+    for i in range(0, len(onset_times)):
+        segment_start = onset_times[i]*sr
+        if i != len(onset_times)-1:
+            segment_end = (onset_times[i+1]*sr)-1
+        else:
+            segment_end = len(signal)-1
+        segments.append(signal[segment_start: segment_end])
+    return segments, onset_times
 
 
-def main():
+def main(argv):
 
-    y, sr = librosa.load('samples/segmenter-tests/psh.wav')
-    segment_audio(y, sr)
+    if argv:
+        path = argv[0]
+    else:
+        path = 'samples/segmenter-tests/test1.wav'
+    y, sr = librosa.load(path, sr=None)
+    segments, times = segment_audio(y, sr)
+
+    for i in range(0, len(segments)):
+        print 'plotting segment: '+str(1+i)+' at time: '+str(times[i])
+        starttime = times[i]
+        endtime = len(segments[i])/float(sr) + times[i]
+        time = np.linspace(starttime, endtime, num=len(segments[i]))
+        plt.plot(time, segments[i])
+        plt.title('Segment '+str(i+1))
+        plt.xlabel('time(s)')
+        plt.ylabel('amplitude')
+        plt.grid(True)
+        plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
