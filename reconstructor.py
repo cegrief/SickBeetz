eb88f0bfec5f79fd0d9e437fe0d4f6676378dfc6
friendly-flame <AdamSnyder2016@u.northwestern.edu>
2015-03-22 15:34:48 -0400

    Output of correct length

--- reconstructor.py
+++ reconstructor.py
@@ -3,10 +3,17 @@ import numpy as np
 
 # replaces each segment with its corresponding replacement, and returns the updated sound
 def replace(times, replacements, sr):
-    out = np.zeros(times[-1]*sr+len(replacements[-1])*2)
+    out = initialize_output(times, replacements, sr)
     for i in range(0, len(replacements)):
         start = times[i]*sr
         end = start + len(replacements[i])
         for j in range(int(start), int(end)):
             out[j] += replacements[i][j-start]
     return out
+
+
+def initialize_output(times, replacements, sr):
+    end_times = [len(times)]
+    for i in range(len(times)):
+        end_times[i] = sr*times[i] + len(replacements[i])
+    return np.zeros(max(end_times)+1)
