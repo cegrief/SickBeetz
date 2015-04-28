cdfc38f0e621890da56eb3d7425c252717140e17
cegrief <cegrief@gmail.com>
2015-04-28 01:26:35 -0500

    fixed form submission error

--- web/routes/services.js
+++ web/routes/services.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 
     app.post('/sickBeetz', function(req,res){
         console.log(req.files);
-        var inp =req.files.thumbnail.path;
+        var inp =req.files.audio.path;
         PythonShell.run('sickBeetz.py', {args: [inp,'kit_1'], scriptPath:'../', pythonOptions: ['-W ignore']}, function(err, results){
             console.log(results);
             res.download(inp.substring(0, inp.length - 4) + '-out.wav')
